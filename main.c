ceee53158b969bcd702587ce68e8fc921753887b
emile-ross <emileross@tutanota.de>
2026-08-26 11:37:44 -0400

    init

---
+++ main.c
@@ -0,0 +1,22 @@
+#include "header.h"
+
+int main(int argc, char *argv[])
+{
+	if (!(argc > 2))
+	{
+		fprintf(stderr, "Missing arguments in command\n");
+		return 1;
+	}
+	
+	uint8_t i = 1;	/* ignore first argument (arg 0) */
+	
+	for (; i < (argc - 1); i++)
+	{
+		if (strcmp(argv[]))
+		{
+		}
+	}
+
+	return 0;
+}
+
