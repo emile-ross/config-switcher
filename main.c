cff67787c0a2b913339f3fdcd378599aa172eeac
emileross <emileross@tutanota.de>
2026-08-26 21:53:07 -0400

    fix: var declarations in code (C99 feature)
    also adds a gitignore file

--- main.c
+++ main.c
@@ -4,13 +4,14 @@
 
 int main(int argc, char *argv[])
 {
+	uint8_t i = 1;	/* ignore first argument (arg 0) */
+
 	if (!(argc > 2))
 	{
 		fprintf(stderr, "Missing arguments in command\n");
 		return 1;
 	}
 	
-	uint8_t i = 1;	/* ignore first argument (arg 0) */
 	
 	for (; i < (argc - 1); i++)
 	{
