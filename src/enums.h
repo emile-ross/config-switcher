0bea4f3ca31a2e3b5d99c482ddaf2a422d92ce86
emileross <emileross@tutanota.de>
2026-08-30 21:47:55 -0400

    refactor: create enums.h
    used for defining enums

---
+++ src/enums.h
@@ -0,0 +1,11 @@
+
+typedef enum
+{
+	INVALID_ERR = 0,
+	ARG_MISSING = 1,
+	MISSING_INPUT_FILES = 2,
+	BUF_TRUNCATION = 21,
+	LSTAT_FAIL = 22,
+	INVALID_ARGS = 40,
+	MALLOC_FAIL = 41
+} err_type;
--- src/header.h
+++ src/header.h
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#include "enums.h"
+
 typedef enum
 {
 	False = 0,
@@ -14,17 +16,6 @@ typedef struct
 	char *config_name;
 	char *style_name;
 } command_args;
-
-typedef enum
-{
-	INVALID_ERR = 0,
-	ARG_MISSING = 1,
-	MISSING_INPUT_FILES = 2,
-	BUF_TRUNCATION = 21,
-	LSTAT_FAIL = 22,
-	INVALID_ARGS = 40,
-	MALLOC_FAIL = 41
-} err_type;
 
 /* compare.c */
 	Bool cmp(char *arg, const char *s_one, const char *s_two);
