d4efd90f71e7bd4ad652274fbb4bd6a65e6faed8
emileross <emileross@tutanota.de>
2026-08-30 22:04:09 -0400

    refactor: move all enums & structs to enums.h
    also fixes the newlines in the header.h file

--- src/enums.h
+++ src/enums.h
@@ -9,3 +9,16 @@ typedef enum
 	INVALID_ARGS = 40,
 	MALLOC_FAIL = 41
 } err_type;
+
+typedef enum
+{
+	False = 0,
+	True = 1
+} Bool;
+
+typedef struct
+{
+	char *program_name;
+	char *config_name;
+	char *style_name;
+} command_args;
--- src/header.h
+++ src/header.h
@@ -4,19 +4,6 @@
 
 #include "enums.h"
 
-typedef enum
-{
-	False = 0,
-	True = 1
-} Bool;
-
-typedef struct
-{
-	char *program_name;
-	char *config_name;
-	char *style_name;
-} command_args;
-
 /* compare.c */
 	Bool cmp(char *arg, const char *s_one, const char *s_two);
 	Bool scmp(char *arg, char *s);
@@ -29,9 +16,8 @@ typedef struct
 
 char *bmalloc(char *fmt, ...);
 
-extern const char *path_to_config;
-
-
 /* switch.c */
 	void switch_config(const char *src, const char *dst);
+
+extern const char *path_to_config;
 
