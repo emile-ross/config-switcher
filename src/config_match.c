e37b5307be32eab15839c41875eb0eb75ae5cfcc
emileross <emileross@tutanota.de>
2026-08-31 21:48:10 -0400

    feat: config_match.c add programs
    adds many programs along with their config name in the config_match.c
    file

--- src/config_match.c
+++ src/config_match.c
@@ -11,6 +11,22 @@ char *get_config_name(const char *restrict program_name, Bool *success)
 		return "init.lua";
 	else if (cmp(program_name, "hypr", "hyprland"))
 		return "hyprland.conf";
+	else if (ch("mpv"))
+		return "mpv.conf";
+	else if (ch("lazygit"))
+		return "config.yml";
+	else if (ch("fuzzel"))
+		return "fuzzel.ini";
+	else if (ch("neofetch"))
+		return "config.conf";
+	else if (ch("htop"))
+		return "htoprc";
+	else if (ch("fastfetch"))
+		return "config.jsonc";
+	else if (ch("kitty"))
+		return "kitty.conf";
+	else if (ch("btop"))
+		return "btop.conf";
 	else if (ch("waybar"))
 		return "config.jsonc";
 	else if (ch("rofi"))
