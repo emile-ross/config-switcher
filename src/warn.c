98aa821d0ee6010c81a0047d2cc3f71530401891
emileross <emileross@tutanota.de>
2026-09-03 16:37:47 -0400

    feat: create warn.c file & warn() function
    adds the warn() function used for displaying a warning on the screen

--- src/warn.c
+++ src/warn.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+#include <stdarg.h>
+
+void warn(const char *restrict fmt, ...)
+{
+	va_list args, copy;
+	va_start(args, fmt);
+	va_copy(copy, args);
+	unsigned int size = (unsigned)vsnprintf(NULL, 0, fmt, copy);
+	va_end(copy);
+}
