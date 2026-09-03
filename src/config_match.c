5cdc99c01d2bcde65971ae7da1f675b5dbf6640e
emileross <emileross@tutanota.de>
2026-09-03 15:51:40 -0400

    feat: make use of the export_type variable
    this specifies the type of config being exported by the program
    it is used as an index into the table whenever we're looking up the
    config name

--- src/config_match.c
+++ src/config_match.c
@@ -2,21 +2,24 @@
 
 #include "table.h"
 
-char *get_config_name(const char *restrict program_name, Bool *success)
+char *get_config_name(const char *restrict program_name, Bool *success, enum config_switch_type export_type)
 {
 	*(success) = True;
 
 	/* get the total number of configs (in the program) */
 	uint16_t num_configs = sizeof(program_configs) / sizeof(program_configs[0]);
 
-	for (uint16_t i = 0; i < num_configs; i++)
+	if (export_type == config)
 	{
-		if (scmp(program_name, (program_configs[i]).program_name))
+		for (uint16_t i = 0; i < num_configs; i++)
 		{
-			*(success) = True;
-			/* TODO make use of the style name 
-			 * the program currently defaults to style.css */
-			return program_configs[i].config_name;
+			if (scmp(program_name, (program_configs[i]).program_name))
+			{
+				*(success) = True;
+				/* TODO make use of the style name 
+				 * the program currently defaults to style.css */
+				return program_configs[i].config_name;
+			}
 		}
 	}
 
