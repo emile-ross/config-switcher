169d6bf258913cac792278a6bce366a681133b6e
emileross <emileross@tutanota.de>
2026-09-03 16:42:57 -0400

    feat: warn.c warn() add buffer write logic
    writes to the memory buffer using vsnprintf() and allocating using
    malloc()

--- src/warn.c
+++ src/warn.c
@@ -6,6 +6,14 @@ void warn(const char *restrict fmt, ...)
 	va_list args, copy;
 	va_start(args, fmt);
 	va_copy(copy, args);
+	/* get the string length using vsnprintf() */
 	unsigned int size = (unsigned)vsnprintf(NULL, 0, fmt, copy);
 	va_end(copy);
+
+	/* allocate memory for the string */
+	char *str = malloc(size);
+	int ret = snprintf(str, size, fmt, args);
+
 }
+
+
