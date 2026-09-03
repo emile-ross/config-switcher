cdda8642cc8e4118a050ab7761afd7b9d6b57ead
emileross <emileross@tutanota.de>
2026-09-03 16:21:22 -0400

    perf: improve speed by only checking type once
    only checks the config type once before returning the value

--- src/config_match.c
+++ src/config_match.c
@@ -9,29 +9,16 @@ char *get_config_name(const char *restrict program_name, Bool *success, enum con
 	/* get the total number of configs (in the program) */
 	uint16_t num_configs = sizeof(program_configs) / sizeof(program_configs[0]);
 
-	if (export_type == config)
+	for (uint16_t i = 0; i < num_configs; i++)
 	{
-		for (uint16_t i = 0; i < num_configs; i++)
+		if (scmp(program_name, (program_configs[i]).program_name))
 		{
-			if (scmp(program_name, (program_configs[i]).program_name))
+			*(success) = True;
+			/* TODO make use of the style name 
+			 * the program currently defaults to style.css */
+			if (export_type == config)
 			{
-				*(success) = True;
-				/* TODO make use of the style name 
-				 * the program currently defaults to style.css */
 				return program_configs[i].config_name;
-			}
-		}
-	}
-	else
-	{
-		for (uint16_t i = 0; i < num_configs; i++)
-		{
-			if (scmp(program_name, (program_configs[i]).program_name))
-			{
-				*(success) = True;
-				/* TODO make use of the style name 
-				 * the program currently defaults to style.css */
-				return program_configs[i].style_name;
 			}
 		}
 	}
