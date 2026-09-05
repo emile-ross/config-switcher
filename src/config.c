371774b0242a56af54cb8f0c82df13895f851d23
emileross <emileross@tutanota.de>
2026-09-05 00:06:50 -0400

    feat: create config.c file
    used for configuring the program with other pre done configs

---
+++ src/config.c
@@ -0,0 +1,13 @@
+#include "header.h"
+
+static const config_contents program_configs[] = 
+{
+	/* { "default", "old-name.conf", "new-name.conf" }, */
+
+	/* example: the following example will make a pointer from 
+	 * the hyprland.conf file to the hyprland-riced.conf file
+	 * { "default", "hyprland-riced.conf", "hyprland.conf" }, 
+	 *
+	 * You can add multiple files with the same identifier */
+
+};
